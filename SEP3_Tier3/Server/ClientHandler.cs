463732a8aea3bf4614254c4bcff62e7c53a31c95
Fabian Bernhardt <fabian_stefan@rocketmail.com>
2019-11-27 14:21:14 +0100

    sters request

--- SEP3_Tier3/Server/ClientHandler.cs
+++ SEP3_Tier3/Server/ClientHandler.cs
@@ -19,21 +19,10 @@ namespace SEP3_TIER3.Server
             Thread thread = new Thread(new ThreadStart(Run));
             thread.Start();
         }
-        private Request ReceiveRequest(NetworkStream stream)
-        {
-            byte[] receiveLengthBytes = new byte[4];
-            stream.Read(receiveLengthBytes);
-            int receiveLength = BitConverter.ToInt32(receiveLengthBytes, 0);
-            byte[] receiveBytes = new byte[receiveLength];
-            stream.Read(receiveBytes);
-            String rcv = Encoding.ASCII.GetString(receiveBytes);
-            return JsonSerializer.Deserialize<Request>(rcv);
-        }
 
         private void SendPlanes(NetworkStream stream, List<Plane> planes)
         {
-            var requestBack = new Request(Request.TYPE.REQUESTPLANES, planes);
-            var json = JsonSerializer.Serialize(requestBack);
+            var json = JsonSerializer.Serialize(planes);
             int length = Encoding.ASCII.GetByteCount(json);
             byte[] toSendBytes = Encoding.ASCII.GetBytes(json);
             byte[] toSendLengthBytes = BitConverter.GetBytes(length);
@@ -43,7 +32,6 @@ namespace SEP3_TIER3.Server
 
         private void SendNodesWithEdges(NetworkStream stream, List<GroundNodeToSend> nodesToSend)
         {
-            var requestBack = new Request(Request.TYPE.REQUESTGROUNDNODES, nodesToSend);
             var json = JsonSerializer.Serialize(nodesToSend);
             int length = Encoding.ASCII.GetByteCount(json);
             byte[] toSendBytes = Encoding.ASCII.GetBytes(json);
@@ -53,53 +41,20 @@ namespace SEP3_TIER3.Server
         }
         private void Run()
         {
-            try
+            while (true)
             {
-                while (true)
+                NetworkStream stream = client.GetStream();
+                if (serverModel.Planes.Count == 0)
                 {
-                    NetworkStream stream = client.GetStream();
-                    Request request = ReceiveRequest(stream);
-                    if (request.Type == Request.TYPE.REQUESTPLANES)
-                    {
-                        if(serverModel.Planes.Count == 0)
-                        {
-                            serverModel.LoadPlanesWithPositionAndPlan();
-                        }
-                        SendPlanes(stream, serverModel.Planes);
-                    }
-                    if(request.Type == Request.TYPE.REQUESTGROUNDNODES)
-                    {
-                        if (serverModel.GroundNodesToSend.Count == 0)
-                        {
-                            serverModel.LoadNodesWithEdgeAndPosition();
-                            serverModel.CreateNodesToSend();
-                        }
-                        SendNodesWithEdges(stream, serverModel.GroundNodesToSend);
-                    }
+                    serverModel.LoadPlanesWithPositionAndPlan();
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-
-        public class Request
-        {
-            public enum TYPE { REQUESTPLANES, REQUESTGROUNDNODES}
-            public TYPE Type { get; }
-            private List<Plane> planes;
-            private List<GroundNodeToSend> nodes;
-
-            public Request(TYPE type, List<Plane> planes)
-            {
-                this.Type = type;
-                this.planes = planes;
-            }
-            public Request(TYPE type, List<GroundNodeToSend> nodes)
-            {
-                this.Type = type;
-                this.nodes = nodes;
+                SendPlanes(stream, serverModel.Planes);
+                if (serverModel.GroundNodesToSend.Count == 0)
+                {
+                    serverModel.LoadNodesWithEdgeAndPosition();
+                    serverModel.CreateNodesToSend();
+                }
+                SendNodesWithEdges(stream, serverModel.GroundNodesToSend);
             }
         }
     }
